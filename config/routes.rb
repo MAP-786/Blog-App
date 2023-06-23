3f8e2e07d89f8e96873e4c22fea269adea1f4fa6
Mohsin <mohsinpanjwani5@gmail.com>
2023-06-23 17:20:40 -0400

    Blog App files

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+end
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
+
